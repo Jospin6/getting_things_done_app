e403c22acf5cd347ea906db8baa416d0ed3c2e20
jospin6 <jospinndagano1@gmail.com>
2024-04-05 13:10:49 +0200

    Redesign the navbar color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
-    return <div className="w-full h-[50px] bg-blue-900 text-white flex justify-between">
+    return <div className="w-full h-[50px] bg-blue-500 text-white flex justify-between">
         <div className="h-[50px] leading-[50px] verdana pl-10">Getting Things Done</div>
-        <button className="px-[10px] bg-blue-500 h-[30px] mt-[10px] mr-[20px] rounded-lg sans-serif text-[15px]">
+        <button className="px-[10px] bg-red-500 h-[30px] mt-[10px] mr-[20px] rounded-lg sans-serif text-[15px]">
             New Project
         </button>
     </div>
