4327b55ecc2cd84df4c7e9397555a64c039bfd40
jospin6 <jospinndagano1@gmail.com>
2024-04-05 13:06:05 +0200

    Create the projectItem component

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,5 @@
+export const ProjectItem = () => {
+    return <div className="col-span-2 min-h-[300px] mt-2 h-auto bg-white rounded-lg flex justify-center">
+
+    </div>
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
-
+import { ProjectItem } from './ProjectItem'
 export const Projects = () => {
-    return <div className="w-full min-h-[300px] h-auto bg-red-500">
-
+    return <div className='px-[50px] fixed top-0 left-0 h-[92vh] w-full mt-[50px] overflow-x-auto'>
+        <div className="w-full min-h-[300px] h-auto grid grid-cols-6 gap-4">
+            <ProjectItem/>
+        </div>
     </div>
 }
