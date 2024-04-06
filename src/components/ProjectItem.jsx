84ae001c6584a3b0b8b0009a07381ddcfcf9a952
jospin6 <jospinndagano1@gmail.com>
2024-04-06 19:46:06 +0200

    Design the header of the project item

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,5 +1,11 @@
 export const ProjectItem = () => {
-    return <div className="md:col-span-2 col-span-1 min-h-[300px] mt-2 h-auto bg-white rounded-lg flex justify-center">
+    return <div className="md:col-span-2 col-span-1 min-h-[300px] mt-2 h-auto 
+    bg-white rounded-lg flex justify-center">
+        <div className="w-full h-[50px] text-center border-b-[1px] 
+        border-gray-400 rounded-t-lg text-black leading-[50px] relative">
+            <span className="font-bold">My Project name</span>
+            <button className="absolute top-0 right-[15px] text-black">ico</button>
+        </div>
 
     </div>
 }
