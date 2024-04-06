791e10ae234c3112328ac82864f32d31c4b94663
jospin6 <jospinndagano1@gmail.com>
2024-04-06 15:49:12 +0200

    Add media queries to the project item

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,5 +1,5 @@
 export const ProjectItem = () => {
-    return <div className="col-span-2 min-h-[300px] mt-2 h-auto bg-white rounded-lg flex justify-center">
+    return <div className="md:col-span-2 col-span-1 min-h-[300px] mt-2 h-auto bg-white rounded-lg flex justify-center">
 
     </div>
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import { ProjectItem } from './ProjectItem'
 export const Projects = () => {
     return <div className='px-[50px] fixed top-0 left-0 h-[92vh] w-full mt-[50px] overflow-x-auto'>
-        <div className="w-full min-h-[300px] h-auto grid grid-cols-6 gap-4">
+        <div className="w-full min-h-[300px] h-auto grid lg:grid-cols-6 md:grid-cols-4 grid-cols-1 md:gap-4">
             <ProjectItem/>
         </div>
     </div>
