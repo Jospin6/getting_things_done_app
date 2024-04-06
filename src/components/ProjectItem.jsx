9ace5f7c87d9e65363e6e398b3bab0ca1c7cb6d6
jospin6 <jospinndagano1@gmail.com>
2024-04-06 22:03:26 +0200

    Design the task item by adding some tailwindcss style

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,11 +1,13 @@
+import { TaskItem } from './TaskItem'
 export const ProjectItem = () => {
     return <div className="md:col-span-2 col-span-1 min-h-[300px] mt-2 h-auto 
-    bg-white rounded-lg flex justify-center">
+    bg-white rounded-lg">
         <div className="w-full h-[50px] text-center border-b-[1px] 
         border-gray-400 rounded-t-lg text-black leading-[50px] relative">
-            <span className="font-bold">My Project name</span>
-            <button className="absolute top-0 right-[15px] text-black">ico</button>
+            <span className="semi-bold text-[20px]">My Project name</span>
+            <button className="absolute top-0 right-[15px] text-black semi-bold">X</button>
         </div>
+        <TaskItem/>
 
     </div>
 }
