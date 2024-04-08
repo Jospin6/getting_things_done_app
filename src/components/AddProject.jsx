f249963095240ab23ad702d5349c393d9612eae3
jospin6 <jospinndagano1@gmail.com>
2024-04-08 20:58:37 +0200

    Put the add Project popup item at the center of the screen

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,7 +1,7 @@
 
 export const AddProject = (props) => {
     const {} = props
-    return <div className="w-[300px] h-[200px] bg-white rounded-lg m-auto">
+    return <div className="w-[300px] h-[200px] mt-[150px] bg-white rounded-lg m-auto">
 
     </div>
 }
