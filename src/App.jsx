f281c4636d6a4a34894be664b94fa70d87c18515
jospin6 <jospinndagano1@gmail.com>
2024-04-08 20:50:20 +0200

    Design the background of the popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Navbar } from './components/Navbar'
 import { Projects } from './components/Projects'
+import { CreateProjectPopup } from './components/CreateProjectPopup'
 
 function App() {
 
   return (
     <>
-      <div className='backgroundImage w-full h-[100vh]'>
+      <div className='backgroundImage w-full h-[100vh] relative'>
         <Navbar/>
         <Projects/>
+        <CreateProjectPopup/>
       </div>
     </>
   )
--- src/components/CreateProjectPopup.jsx
+++ src/components/CreateProjectPopup.jsx
@@ -1,6 +1,6 @@
 
 export const CreateProjectPopup = () =>{
-    return <div className="">
+    return <div className="balck-opacity w-full h-[100vh] absolute right-0 top-0">
 
     </div>
 }
