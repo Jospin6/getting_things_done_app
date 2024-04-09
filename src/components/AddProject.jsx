5e8af16ba1a9e4251a04da483a6054784d809e81
jospin6 <jospinndagano1@gmail.com>
2024-04-09 20:19:51 +0200

    Design the create project item header

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -2,6 +2,8 @@
 export const AddProject = (props) => {
     const {} = props
     return <div className="w-[300px] h-[200px] mt-[150px] bg-white rounded-lg m-auto">
-
+        <div className="w-full h-[40px] rounded-t-lg text-center leading-[40px] semi-bold text-[20px]">
+            New Project
+        </div>
     </div>
 }
