b997c6db0a95604a1556ea109d433dbaf249d87e
jospin6 <jospinndagano1@gmail.com>
2024-04-12 15:55:20 +0200

    Add to key word to the maped project item

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ export const Projects = () => {
     return <div className='px-[50px] fixed top-0 left-0 h-[92vh] w-full mt-[50px] overflow-x-auto'>
         <div className="w-full min-h-[300px] h-auto grid lg:grid-cols-6 md:grid-cols-4 grid-cols-1 md:gap-4">
             {
-                projects.map(project => <ProjectItem project={project}/>)
+                projects.map(project => <ProjectItem project={project} key={project.id}/>)
             }
             
         </div>
